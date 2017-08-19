f7524e751b7d93a8a23406ff058562f72dbab5c2
Semir <semir.teskeredzic@gmail.com>
2017-08-19 15:52:24 +0200

    whole number function

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,4 +6,9 @@ function guess() {
     //add functionality to guess function here
 }
 
+function setHiddenFields(){
+  var wholenum = Math.floor(input);
+  var answer = wholenum.Math.random()
+}
+
 //implement new functions here
