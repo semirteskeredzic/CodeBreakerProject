4fdb1b12ef53a8c117b16510b94b099f65682339
Semir <semir.teskeredzic@gmail.com>
2017-08-19 15:59:49 +0200

    rev3

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,12 +3,16 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
+
+    function setHiddenFields(){
+      var answer = Math.floor(Math.random() * 9999);
+    }
     //add functionality to guess function here
 }
 
-function setHiddenFields(){
-  var answer = Math.floor(Math.random() * 9999);
-}
+//function setHiddenFields(){
+  //var answer = Math.floor(Math.random() * 9999);
+//}
 
 
 
